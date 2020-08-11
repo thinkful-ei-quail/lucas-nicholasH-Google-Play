af206c2c7201031372f3c4dd879f9ce8b6cb289e
Lucas Bigelow <lukejbigelow@gmail.com>
2020-08-11 10:09:12 -0600

    added sort query functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const morgan = require('morgan');
 
@@ -10,6 +12,27 @@ const apps = require('./playstore.js');
 
 app.get('/apps', (req, res) => {
     const { sort, genres } = req.query;
+
+    const sortedApps = [...apps];
+
+    const sortVals = ['Rating', 'App'];
+
+    if(sort) {
+        if(!sortVals.includes(sort)) {
+            res.status(400).send('Please send only a "rating" or "app"');
+        }
+        sortedApps.sort((a, b) => {
+            if (a[sort] < b[sort]) {
+                return -1;
+            } else if (a[sort] > b[sort]) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        res.json(sortedApps);
+    }
 
     res.json(apps);
 
